7c46a16399c0c2bfed0326e353184da99b8fa286
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-04-14 11:56:18 +0100

    add folder img, add component for welcomepg, add style css

--- src/Pages/WelcomePage/Components/WaterBenefitsSection/WaterBenefitsSection.styled.jsx
+++ src/Pages/WelcomePage/Components/WaterBenefitsSection/WaterBenefitsSection.styled.jsx
@@ -1,6 +1,45 @@
 import styled from 'styled-components';
 
-export const WaterBenefitsContainer = styled.div``;
-export const WaterTitle = styled.h2``;
-export const BenefitsList = styled.ul``;
-export const BenefitsLi = styled.li``;
+export const WaterBenefitsContainer = styled.div`
+  background-color: #ecf2ff;
+  padding: 24px 16px;
+  border-radius: 10px;
+  gap: 12px;
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    padding: 32px 24px;
+    width: 446px;
+  }
+  @media screen and (min-width: 1440px) {
+    margin-top: 34px;
+  }
+`;
+export const WaterTitle = styled.h2`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 1.1em;
+  margin-bottom: 12px;
+  margin-top: 0px;
+`;
+export const BenefitsList = styled.ul`
+  padding-inline-start: 0px;
+  margin: 0px;
+`;
+export const BenefitsLi = styled.li`
+  display: flex;
+  font-size: 16px;
+  gap: 8px;
+  align-items: center;
+  line-height: 1.25em;
+
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+
+  &::before {
+    content: '';
+    color: #407bff;
+    border-style: solid;
+    border-width: 0 8px 8px 0;
+    border-radius: 50%;
+  }
+`;
--- src/Pages/WelcomePage/Components/WaterTracker/WaterTracker.jsx
+++ src/Pages/WelcomePage/Components/WaterTracker/WaterTracker.jsx
@@ -1,3 +1,47 @@
+// import { useNavigate } from 'react-router-dom';
+import {
+  WaterTrackerContainer,
+  WaterTrackerTitle,
+  WaterTrackerInfo,
+  WaterTrackerSubtitle,
+  WaterTrackerList,
+  WaterTrackerLi,
+  Button,
+  CalendarSvg,
+  PersonalSetingsSvg,
+  StatisticsSvg,
+} from './WaterTracker.styled';
+
 export const WaterTracker = () => {
-  return <></>;
+  //   const navigate = useNavigate();
+
+  const onClick = () => {
+    // navigate('/signUp');
+  };
+
+  return (
+    <WaterTrackerContainer>
+      <WaterTrackerTitle>Water consumption tracker</WaterTrackerTitle>
+      <WaterTrackerInfo>Record daily water intake and track</WaterTrackerInfo>
+      <WaterTrackerSubtitle>Tracker Benefits</WaterTrackerSubtitle>
+      <WaterTrackerList>
+        <WaterTrackerLi>
+          <CalendarSvg />
+          Habit drive
+        </WaterTrackerLi>
+        <WaterTrackerLi>
+          <StatisticsSvg />
+          View statistics
+        </WaterTrackerLi>
+        <WaterTrackerLi>
+          {' '}
+          <PersonalSetingsSvg />
+          Personal rate setting
+        </WaterTrackerLi>
+      </WaterTrackerList>
+      <Button type="button" onClick={onClick}>
+        Try tracker
+      </Button>
+    </WaterTrackerContainer>
+  );
 };
