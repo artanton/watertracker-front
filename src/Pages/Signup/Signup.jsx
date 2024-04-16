523a6e6252dd851b5b6ec74de7b5198d1ccb16df
VadymRyzhuk <ceasarj571@gmail.com>
2024-04-16 22:14:12 +0200

    add store/signUp/authReducer

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,12 +1,56 @@
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+//import { apiRegisterUser } from './redux/authorization/authReducer';
 
-function Signup() {
+const Signup = () => {
+  const dispatch = useDispatch();
+
+  const onSubmitRegistr = event => {
+    event.preventDefault();
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const repeatPassword = event.currentTarget.elements.repeatPassword.value;
+
+    const formData = {
+      email,
+      password,
+      repeatPassword,
+    };
+    //console.log(formData);
+    // dispatch(apiRegisterUser(formData));
+    // .unwrap()
+    // .then(() => toast.success('Perfect! Successfully registration!'))
+    // .catch(() => toast.error('Please, write a correct email or password!'));
+    //// event.currentTarget.reset();
+  };
+
   return (
     <>
       <div>SIGNUP PAGE HERE</div>
       <Link to="/signin">Signin</Link>
+
+      <form onSubmit={onSubmitRegistr}>
+        <label>
+          Enter your email
+          <input type="text" name="email" placeholder="Email" required />
+        </label>
+        <label>
+          Enter your password
+          <input type="text" name="password" placeholder="Password" required />
+        </label>
+        <label>
+          Repeat password
+          <input
+            type="text"
+            name="repeatPassword"
+            placeholder="Repeat password"
+            required
+          />
+        </label>
+        <button type="submit">Sign up</button>
+      </form>
     </>
   );
-}
+};
 
 export default Signup;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { persistor } from './redux/store';
+import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/final-team-project-group07">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/final-team-project-group07">
+        <PersistGate persistor={persistor} loading={null}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
