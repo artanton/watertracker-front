95b9e1548585ed7a30d13aa1f4016940c7012ee0
Anton Pedan <77937504+snap515@users.noreply.github.com>
2024-04-17 20:33:06 +0200

    adds parent modal component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,26 @@
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../redux/modal/modalSlice';
+import { Modal, CloseModalCross } from 'components';
+import { modalNames } from 'constants/constants';
 function Home() {
+  const dispatch = useDispatch();
   return (
     <div>
       HOME PAGE HERE. This page wants a private route. Please add a private
       route after adding a redux in project. And this page should be on another
       route.
+      <button
+        type="button"
+        onClick={() => dispatch(openModal(modalNames.ADD_WATER))}
+      >
+        OPEN MODAL BUTTON
+      </button>
+      <Modal modalId={modalNames.ADD_WATER}>
+        <div>
+          <CloseModalCross />
+          Modal content here
+        </div>
+      </Modal>
     </div>
   );
 }
