f25dd5c29920581342a4bc0677bb0a9c6755052b
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-04-20 23:14:18 +0100

    add theme

--- src/components/YesNoButton/YesNoButton.styled.jsx
+++ src/components/YesNoButton/YesNoButton.styled.jsx
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
+import { theme } from '../../styles/theme';
 
 export const Button = styled.button`
-  background-color: ${props => (props.approve ? 'red' : 'grey')};
-  color: ${props => (props.approve ? 'white' : 'blue')};
+  background-color: ${props =>
+    props.approve
+      ? '${theme.colors.secondaryRed}'
+      : '${theme.colors.secondaryGray}'};
+  color: ${props =>
+    props.approve
+      ? '${theme.colors.primaryWhite'
+      : '${theme.colors.primaryBlue'};
   align-items: center;
   display: inline-flex;
   jystify-content: center;
@@ -12,7 +19,7 @@ export const Button = styled.button`
   padding: 8px 30px;
   border: none;
   border-radius: 10px;
-  width: 100%;
+  width: 232px
   box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
   &:hover {
     box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
@@ -21,5 +28,6 @@ export const Button = styled.button`
     padding: 10px 30px;
     font-size: 18px;
     line-height: 1.33em;
+    width: 160px;
   }
 `;
