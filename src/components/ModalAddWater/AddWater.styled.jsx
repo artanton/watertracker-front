1517824f1cf440015f809c90788496d57afab749
Андрій <rukom@ukr.net>
2024-04-21 15:18:25 +0300

    Update AddWater.styled.jsx

--- src/components/ModalAddWater/AddWater.styled.jsx
+++ src/components/ModalAddWater/AddWater.styled.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
+import { theme } from 'styles/theme';
 
 export const ModalContainer = styled.div`
   width: 592px;
   height: 592px;
 `;
 
-export const ModalHeader = styled.div``;
+export const ModalHeader = styled.div`
+  display: flex;
+`;
 
-export const ModalTitle = styled.h2``;
+export const ModalTitle = styled.h2`
+  font-size: ${theme.fontSizes.subtitle};
+  font-weight: ${theme.fontWeights.medium};
+`;
