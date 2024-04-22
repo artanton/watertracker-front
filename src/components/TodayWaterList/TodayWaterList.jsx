f9e96da69444d87a6968a27ad8374261774962b5
anetten <anet.zvekova@gmail.com>
2024-04-22 15:05:56 +0300

    add WaterList and Calendar

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,34 +1,66 @@
 import React from 'react';
 import {
   AddWaterButton,
-  // PlusIcon,
+  DeleteButton,
+  EditButton,
+  IconsContainer,
   Today,
   TodayWaterListContainer,
-  // WaterList,
-  // WaterListUl,
+  WaterDeleteIcon,
+  WaterEditIcon,
+  WaterGlassIcon,
+  WaterList,
+  WaterListItem,
+  WaterListMl,
+  WaterListTime,
 } from './TodayWaterList.styled';
 import { Plus } from 'components/Icons/Plus/Plus';
 // import { useDispatch } from 'react-redux';
 
+// const WaterListDisplay = () => {}
+
 // const WaterItem = ({id, ml, time}) => {
 //   const dispatch = useDispatch();
-//   const handleEditBtnClick = () => {}
-//   const handleDeleteBtnClick = () =>{}
-//   return ()
-// }
 
-// const WaterList = () => {
-//   return (
-//     <WaterListUl>
-
-//     </WaterListUl>
-//   )
+//   const handleEditBtn = (id) => {
+// const edit = edit(?) (id);
+// dispatchEvent(action);
 // }
+//   const handleDeleteBtn = (id) =>{
+//const delete = delete(?)(id);
+// dispatch(delete)
+// };}
 
 const TodayWaterList = ({ HandleOpenModal }) => {
   return (
     <TodayWaterListContainer>
       <Today>Today</Today>
+      <WaterList>
+        <WaterListItem>
+          <WaterGlassIcon />
+          <WaterListMl>
+            {/* {ml} */}
+            200 ml
+          </WaterListMl>
+          <WaterListTime>
+            {/* {time} */}
+            14:00 PM
+          </WaterListTime>
+          <IconsContainer>
+            <EditButton
+            // onClick={HandleEditBtn}
+            >
+              <WaterEditIcon />
+            </EditButton>
+
+            <DeleteButton
+            // onClick={HandleDeleteBtn}
+            >
+              <WaterDeleteIcon />
+            </DeleteButton>
+          </IconsContainer>
+        </WaterListItem>
+      </WaterList>
       <AddWaterButton onClick={HandleOpenModal}>
         <Plus />
         Add water
--- src/components/TodayWaterList/TodayWaterList.styled.jsx
+++ src/components/TodayWaterList/TodayWaterList.styled.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import { theme } from 'styles/theme';
 import { Plus } from 'components/Icons/Plus/Plus';
+import { GlassOfWater } from 'components/Icons/GlassOfWater';
+import { PencilSquare } from 'components/Icons/PencilSquare';
+import { Delete } from 'components/Icons/Delete';
 
 export const TodayWaterListContainer = styled.div`
   width: 264px;
@@ -12,6 +15,8 @@ export const TodayWaterListContainer = styled.div`
   }
 
   @media (min-width: 1440px) {
+    width: 544px;
+    height: 260px;
   }
 `;
 export const Today = styled.h2`
@@ -50,14 +55,178 @@ export const AddWaterButton = styled.button`
     color: ${theme.colors.secondaryOrange};
   }
 `;
-export const WaterListUl = styled.ul`
-  height: 212px;
-  width: 100%;
+
+export const WaterList = styled.ul`
+  max-height: 188px;
+  width: 264px;
+  padding-left: 0;
+  overflow-y: auto;
+  margin-top: 16px;
+
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  &::-webkit-scrollbar-track {
+    margin-top: 8px;
+    margin-right: 2px;
+    margin-left: 4px;
+    width: 4px;
+    border-radius: 8px;
+    background-color: #d7e3ff;
+    border-style: solid;
+    border-color: #ecf2ff;
+    border-width: 1px 1px 1px 1px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    width: 8px;
+    background-color: #9ebbff;
+    border-radius: 8px;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    width: 656px;
+    max-height: 176px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 544px;
+    max-height: 176px;
+  }
 `;
 
-export const WaterItem = styled.li`
+export const WaterListItem = styled.li`
+  display: flex;
   height: 50px;
-  border-top: 1px solid ${theme.colors.secondaryGrayBlue};
-  border-bottom: 1px solid ${theme.colors.secondaryGrayBlue};
+  width: 254px;
   align-items: center;
+  border-top: 1px solid #d7e3ff;
+  border-bottom: 1px solid #d7e3ff;
+
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    width: 646px;
+    height: 60px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 534px;
+    height: 60px;
+  }
+`;
+
+export const WaterGlassIcon = styled(GlassOfWater)`
+  width: 26px;
+  height: 26px;
+
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    width: 36px;
+    height: 36px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 36px;
+    height: 36px;
+  }
+`;
+
+export const WaterListMl = styled.p`
+  width: 60px;
+  font-weigth: 400px;
+  font-size: 18px;
+  line-height: 24px;
+  color: #407bff;
+  margin-left: 12px;
+  margin-right: 12px;
+
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    margin-right: 16px;
+  }
+  @media screen and (min-width: 1440px) {
+    margin-right: 16px;
+  }
+`;
+
+export const WaterListTime = styled.p`
+  width: 56px;
+  font-weight: 400px;
+  font-size: 12px;
+  line-height: 24px;
+  color: #2f2f2f;
+
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    width: 82px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 82px;
+  }
+`;
+
+export const IconsContainer = styled.div`
+  display: flex;
+  alig-items: center;
+  margin-left: 38px;
+  gap: 18px;
+
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    margin-left: 390px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    margin-left: 278px;
+  }
+`;
+
+export const EditButton = styled.button`
+  background-color: transparent;
+  border: none;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: ${theme.colors.secondaryBlue};
+    transform: scaleX(0);
+    transition: transform 0.3s ease;
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
+`;
+
+export const DeleteButton = styled.button`
+  background-color: transparent;
+  border: none;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: ${theme.colors.secondaryRed};
+    transform: scaleX(0);
+    transition: transform 0.3s ease;
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
+`;
+
+export const WaterEditIcon = styled(PencilSquare)`
+  width: 16px;
+  height: 16px;
+`;
+
+export const WaterDeleteIcon = styled(Delete)`
+  width: 16px;
+  height: 16px;
 `;
