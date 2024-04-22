0c9ffcb8923f737e6f32a3721ed5c7cdd19d3f37
Андрій <rukom@ukr.net>
2024-04-22 16:28:23 +0300

    Update AddWaterForm.jsx

--- src/components/ModalAddWater/AddWaterForm/AddWaterForm.jsx
+++ src/components/ModalAddWater/AddWaterForm/AddWaterForm.jsx
@@ -6,8 +6,13 @@ import {
 } from './AddWaterForm.styled';
 import { Minus } from 'components/Icons/Minus';
 import { Plus } from 'components/Icons/Plus/Plus';
+import { Form, Formik } from 'formik';
 
 export const AddWaterForm = () => {
+  const handleSubmit = values => {
+    console.log('values:', values);
+  };
+
   return (
     <>
       <LabelCount>Amount of water:</LabelCount>
@@ -20,6 +25,12 @@ export const AddWaterForm = () => {
           <Plus />
         </ButtonChange>
       </CountContainer>
+      <Formik
+        initialValues={{ time: '', quantity: '' }}
+        onSubmit={handleSubmit}
+      >
+        {({ errors }) => <Form autoComplete="off"></Form>}
+      </Formik>
     </>
   );
 };
