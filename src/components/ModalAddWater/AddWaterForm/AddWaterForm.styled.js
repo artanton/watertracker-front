c0da85eb2e5e55e891e35b04e65b4ff1e0d1793b
Андрій <rukom@ukr.net>
2024-04-22 15:21:32 +0300

    Update AddWaterForm.styled.js

--- src/components/ModalAddWater/AddWaterForm/AddWaterForm.styled.js
+++ src/components/ModalAddWater/AddWaterForm/AddWaterForm.styled.js
@@ -1,7 +1,40 @@
 import styled from "styled-components";
+import { theme } from "styles/theme";
+import { Minus } from "components/Icons/Minus";
 
-export const CountContainer = styled.div``;
+export const CountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 7px;
+`;
 
-export const ButtonChange = styled.button``;
+export const ButtonChange = styled.button`
+  width: 44px;
+  height: 44px;
+  border-radius: 50%;
+  color: ${theme.colors.primaryBlue};
+  border: 1px solid ${theme.colors.primaryBlue};
+  background-color: transparent;
+  text-align: center;
+`;
 
-export const CountValue = styled.div``;
+export const MinusIcon = styled(Minus)`
+  margin: 0 auto;
+`;
+
+export const LabelCount = styled.p`
+  margin-bottom: 12px;
+`;
+
+export const CountValue = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 92px;
+  height: 36px;
+  font-size: ${theme.fontSizes.listTitle};
+  font-weight: ${theme.fontWeights.bold};
+  color: ${theme.colors.primaryBlue};
+  background-color: ${theme.colors.secondaryGrayBlue};
+  border-radius: 40px;
+`;
