1127c7b6970c6b46c8f0139e35f71488a373af0d
Андрій <rukom@ukr.net>
2024-04-24 09:08:39 +0300

    Update AddWaterForm.jsx

--- src/components/ModalAddWater/AddWaterForm/AddWaterForm.jsx
+++ src/components/ModalAddWater/AddWaterForm/AddWaterForm.jsx
@@ -13,108 +13,91 @@ import {
 } from './AddWaterForm.styled';
 import { Minus } from 'components/Icons/Minus';
 import { Plus } from 'components/Icons/Plus/Plus';
-import { Form, Formik, ErrorMessage } from 'formik';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addWater } from '../../../redux/waterData/thunk';
+import { onChange } from 'react-toastify/dist/core/store';
 
 export const AddWaterForm = () => {
-  const [selectedTime, setSelectedTime] = useState(getDefaultTime());
-  const [count, setCount] = useState(0);
-  const [inputValue, setInputValue] = useState('');
-  const [otherInputValue, setOtherInputValue] = useState('');
+  const [waterCount, setWaterCount] = useState(50);
+  const [timeValue, setTimeValue] = useState(new Date());
+  const dispatch = useDispatch();
 
-  const handleBlur = event => {
-    setOtherInputValue(event.target.value);
+  const hours = timeValue.getHours().toString().padStart(2, '0');
+  const minutes = timeValue.getMinutes().toString().padStart(2, '0');
+
+  const increment = () => {
+    setWaterCount(state => state + 50);
   };
 
   const decrement = () => {
-    setCount(count - 50);
+    setWaterCount(state => Math.max(state - 50));
   };
 
-  const increment = () => {
-    setCount(count + 50);
+  const onChange = event => {
+    setTimeValue(event.target.value);
   };
 
-  const handleSubmit = (values, actions) => {
-    console.log('values:', values);
-    console.log('actions:', actions);
-  };
-
-  function getDefaultTime() {
-    const now = new Date();
-    const hour = now.getHours().toString().padStart(2, '0');
-    const minute = now.getMinutes().toString().padStart(2, '0');
-    return `${hour}:${minute}`;
-  }
-
-  console.log(getDefaultTime());
-
-  const handleChange = event => {
-    setSelectedTime(event.target.value);
+  const handleSubmit = () => {
+    console.log(timeValue);
   };
 
   return (
-    <>
+    <form autoComplete="off" onSubmit={handleSubmit}>
       <LabelCount>Amount of water:</LabelCount>
       <CountContainer>
-        <ButtonChange onClick={decrement}>
+        <ButtonChange
+          type="button"
+          onClick={decrement}
+          disabled={waterCount <= 49 ? true : false}
+        >
           <Minus />
         </ButtonChange>
-        <CountValue>
-          {otherInputValue ? `${otherInputValue}ml` : `${count}ml`}
-        </CountValue>
-        <ButtonChange onClick={increment}>
+        <CountValue>{waterCount}</CountValue>
+        <ButtonChange
+          type="button"
+          onClick={increment}
+          disabled={waterCount >= 1451 ? true : false}
+        >
           <Plus />
         </ButtonChange>
       </CountContainer>
-      <Formik
-        initialValues={{ time: '', quantity: '' }}
-        onSubmit={handleSubmit}
-      >
-        {({ errors }) => (
-          <Form autoComplete="off">
-            <LabelSelect>Recording time:</LabelSelect>
-            <SelectInput
-              name="time"
-              onChange={handleChange}
-              $error={errors.time}
-            >
-              <option>{selectedTime}</option>
-              {Array.from({ length: 12 * 12 }).map((_, index) => {
-                const hour = Math.floor(index / 12);
-                const minute = (index % 12) * 5;
-                const hourStr = hour.toString().padStart(2, '0');
-                const minuteStr = minute.toString().padStart(2, '0');
-                return (
-                  <option key={index} value={`${hourStr}:${minuteStr}`}>
-                    {`${hourStr}:${minuteStr}`}
-                  </option>
-                );
-              })}
-            </SelectInput>
-            <ErrorMessage name="time" component={Error} />
-            <LabelQuantityInput htmlFor="quantity">
-              Enter the value of the water used:
-            </LabelQuantityInput>
-            <QuantityInput
-              name="quantity"
-              type="number"
-              id="quantity"
-              placeholder="0"
-              $error={errors.quantity}
-              value={inputValue}
-              onChange={event => setInputValue(event.target.value)}
-              onBlur={handleBlur}
-            />
-            <ErrorMessage name="quantity" component={Error} />
-            <ButtonContainer>
-              <WaterQuantityValue>
-                {otherInputValue ? `${otherInputValue}ml` : `${count}ml`}
-              </WaterQuantityValue>
-              <SaveModalButton />
-            </ButtonContainer>
-          </Form>
-        )}
-      </Formik>
-    </>
+      <LabelSelect>Recording time:</LabelSelect>
+      <SelectInput name="time" onChange={onChange}>
+        <option value={`${hours}:${minutes}`}>{`${hours}:${minutes}`}</option>
+        {Array.from({ length: 24 * 12 }).map((_, index) => {
+          const hour = Math.floor(index / 12);
+          const minute = (index % 12) * 5;
+          const hourStr = hour.toString().padStart(2, '0');
+          const minuteStr = minute.toString().padStart(2, '0');
+          return (
+            <option key={index} value={`${hourStr}:${minuteStr}`}>
+              {`${hourStr}:${minuteStr}`}
+            </option>
+          );
+        })}
+      </SelectInput>
+      {/* <ErrorMessage name="time" component={Error} /> */}
+      <LabelQuantityInput htmlFor="quantity">
+        Enter the value of the water used:
+      </LabelQuantityInput>
+      <QuantityInput
+        name="number"
+        type="number"
+        id="quantity"
+        placeholder="0"
+        // $error={errors.quantity}
+        // value={inputValue}
+        // onChange={event => setInputValue(event.target.value)}
+        // onBlur={handleBlur}
+      />
+      {/* <ErrorMessage name="quantity" component={Error} /> */}
+      <ButtonContainer>
+        <WaterQuantityValue>
+          {/* {otherInputValue ? `${otherInputValue}ml` : `${count}ml`} */}
+        </WaterQuantityValue>
+        <SaveModalButton />
+      </ButtonContainer>
+    </form>
   );
 };
