c1d894f35d109a398707328b8e2d1a46c84c6135
Андрій <rukom@ukr.net>
2024-04-25 11:20:05 +0300

    Update AddWaterForm.jsx

--- src/components/ModalAddWater/AddWaterForm/AddWaterForm.jsx
+++ src/components/ModalAddWater/AddWaterForm/AddWaterForm.jsx
@@ -45,7 +45,14 @@ export const AddWaterForm = () => {
     const currentDate = new Date();
     currentDate.setHours(hour);
     currentDate.setMinutes(minute);
-    setTimeValue(currentDate);
+    const year = String(currentDate.getFullYear());
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDay()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    const now = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+    setTimeValue(now);
   };
 
   const handleChange = event => {
