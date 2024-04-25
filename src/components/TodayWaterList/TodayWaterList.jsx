7daa95ed22776b37f7e8977f666f0905264c2a9a
Андрій <rukom@ukr.net>
2024-04-25 14:50:45 +0300

    Update TodayWaterList.jsx

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -18,13 +18,14 @@ import {
 import { Plus } from 'components/Icons/Plus/Plus';
 import { openModal } from '../../redux/modal/modalSlice';
 import { modalNames } from 'constants/constants';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectorWaterNotes } from '../../redux/selectors';
+import { ModalEditWater } from 'components/ModalEditWater/ModalEditWater';
 
 const TodayWaterList = () => {
   const dispatch = useDispatch();
 
-  const waterNotes = useSelector(selectorWaterNotes)
+  const waterNotes = useSelector(selectorWaterNotes);
   const formatTime = timeString => {
     const date = new Date(timeString);
     return date.toLocaleTimeString('en-US', {
@@ -46,7 +47,7 @@ const TodayWaterList = () => {
             <IconsContainer>
               <EditButton
                 type="button"
-                // onClick={() => dispatch(openModal())}
+                onClick={() => dispatch(openModal(modalNames.EDIT_WATER))}
               >
                 <WaterEditIcon />
               </EditButton>
@@ -64,6 +65,7 @@ const TodayWaterList = () => {
         <Plus />
         Add water
       </AddWaterButton>
+      <ModalEditWater />
     </TodayWaterListContainer>
   );
 };
