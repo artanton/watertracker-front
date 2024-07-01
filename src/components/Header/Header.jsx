9219af30c3e414efdaceb879cdfa80dcfecacabf
artanton <kabletrade@gmail.com>
2024-07-01 20:44:53 +0300

    Fix signIn header+change baseURL
    signIN header localization fixed
    base url changed
    commented code deleted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,73 +1,4 @@
-// import { Link } from 'react-router-dom';
-
-// import { useSelector } from 'react-redux';
-
-// import { useTranslation } from 'react-i18next';
-
-// import { selectIsLoggedIn, selectUserData } from '../../redux/selectors';
-
-// import { LanguageSelector, PopupUser } from 'components';
-// import { Logo } from 'components/Icons/Logo';
-// import { UserIcon } from 'components/Icons/UserIcon';
-
-// import {
-//   UserLanguagesWrapper,
-//   SignInText,
-//   HeaderContainer,
-//   UserLogoContainer,
-//   UserInfo,
-//   UserAvatar,
-//   UserNameText,
-// } from './Header.styled';
-
-// export const Header = () => {
-//   const isLoggedIn = useSelector(selectIsLoggedIn);
-//   const userData = useSelector(selectUserData);
-//   const userEmail = userData?.email ?? 'Sign in';
-//   const userEmailSplit = userEmail.split('@')[0];
-//   const userName = userData?.userName;
-//   const userAvatar = userData?.avatarURL;
-
-//   const { t } = useTranslation();
-
-//   return (
-//     <>
-//       <HeaderContainer>
-//         <Link to="/">
-//           <Logo />
-//         </Link>
-//         <UserLanguagesWrapper>
-//           <UserInfo>
-//             <Link to="/signin">
-//               <UserLogoContainer>
-//                 {/* <SignInText>Sign in</SignInText> */}
-//                 {isLoggedIn ? (
-//                   userName ? (
-//                     <div>
-//                       <UserNameText>{userName}</UserNameText>
-//                     </div>
-//                   ) : (
-//                     <div>{userEmailSplit}</div>
-//                   )
-//                 ) : (
-//                   <SignInText>{t('userAuthTitle')}</SignInText>
-//                 )}
-//                 {isLoggedIn && userAvatar ? (
-//                   <UserAvatar src={userAvatar} alt="userPhoto" />
-//                 ) : (
-//                   <UserIcon />
-//                 )}
-//               </UserLogoContainer>
-//             </Link>
-//             {isLoggedIn && <PopupUser />}
-//           </UserInfo>
-//           {/* {!isLoggedIn && <LanguageSelector />} */}
-//           <LanguageSelector />
-//         </UserLanguagesWrapper>
-//       </HeaderContainer>
-//     </>
-//   );
-// };
+import { useTranslation } from 'react-i18next';
 
 import { Link } from 'react-router-dom';
 
@@ -86,6 +17,7 @@ import {
   UserInfo,
   UserAvatar,
   UserNameText,
+  SignInText,
 } from './Header.styled';
 
 export const Header = () => {
@@ -95,7 +27,7 @@ export const Header = () => {
   const userEmailSplit = userEmail.split('@')[0];
   const userName = userData?.userName;
   const userAvatar = userData?.avatarURL;
-
+  const { t } = useTranslation();
   return (
     <>
       <HeaderContainer>
@@ -106,15 +38,17 @@ export const Header = () => {
           <UserInfo>
             <Link to="/signin">
               <UserLogoContainer>
-                {/* <SignInText>Sign in</SignInText> */}
-                {isLoggedIn &&
-                  (userName ? (
+                {isLoggedIn ? (
+                  userName ? (
                     <div>
                       <UserNameText>{userName}</UserNameText>
                     </div>
                   ) : (
                     <div>{userEmailSplit}</div>
-                  ))}
+                  )
+                ) : (
+                  <SignInText>{t('authForm.signinTitle')}</SignInText>
+                )}
                 {isLoggedIn && userAvatar ? (
                   <UserAvatar src={userAvatar} alt="userPhoto" />
                 ) : (
@@ -124,7 +58,7 @@ export const Header = () => {
             </Link>
             {isLoggedIn && <PopupUser />}
           </UserInfo>
-          {/* {!isLoggedIn && <LanguageSelector />} */}
+
           {!isLoggedIn && <LanguageSelector />}
         </UserLanguagesWrapper>
       </HeaderContainer>
