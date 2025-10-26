8fd57eada0e6aaf9ad792aed24675c6973604029
anton.kononov <anton.kononov.dev@gmail.com>
2025-10-26 22:31:44 +0200

    vite deploy setting

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/watertracker-front">
+      {/* <BrowserRouter basename="/watertracker-front"> */}
+      <BrowserRouter basename="">
         <PersistGate persistor={persistor} loading={null}>
           <App />
           <ToastContainer
